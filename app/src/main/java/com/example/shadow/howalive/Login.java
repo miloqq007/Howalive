148c466f682a8a3ed2972d2bd91700518ebf204c
Wang <563463424@qq.com>
2016-03-03 18:07:12 +0800

    Test

---
+++ app/src/main/java/com/example/shadow/howalive/Login.java
@@ -0,0 +1,18 @@
+package com.example.shadow.howalive;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Shadow on 2016/3/1.
+ */
+public class Login  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
